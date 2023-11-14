c3dfba23232d74e44e6e3381c7c6c7a9cb008938
Dionathan <dio1nathan2010@gmail.com>
2023-11-14 09:10:32 -0300

    GenerateKey

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 let sizePassWord = document.querySelector("#valor");
 let passWord = document.querySelector("#password");
+let containerPassword=document.querySelector("#container-password")
 
 
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!";
@@ -11,3 +12,17 @@ sizePassWord.innerHTML=sliderElement.value;
  slider.oninput = function(){
      sizePassWord.innerHTML = this.value;
  }
+  
+function generatePassword(){
+    let pass = "";
+    for(let i=0, n=charset.length; i<sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random()*n))
+}
+    containerPassword.classList.remove("hide");
+    passWord.innerHTML=pass;
+    novaSenha=pass;
+}
+function copyPassword(){
+    alert("Senha copiada com sucesso!");
+    navigator.clipboard.writeText(novaSenha);
+}
